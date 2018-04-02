59a4efc16b9b44d31595e2d45910e8bb849e246a
branko <brassta@gmail.com>
2018-04-02 20:34:34 +0200

    commit 02-04 final

--- js/main.js
+++ js/main.js
@@ -5,13 +5,12 @@
  * Time: 11:55 AM
  */
 "use strict";
-(function(){
+(function() {
     var mobileMenuTrigger = $('.navigation-trigger a:last-child');
     var articleContent = $('.article-container.half').find('.article-content');
     var articleTitle = $('.article-container.half').find('h2');
     var highestOne = Number.MIN_VALUE;
-
-
+    var loadMoreToggleButton = $('.load-more-button-container  a');
 
     /**
      * events
@@ -20,7 +19,7 @@
     var headerContainer = $('.head-content-wrapper');
     var footerContainer = $('.footer-content-wrapper');
 
-    headerContainer.load('header.html',function(){
+    headerContainer.load('header.html', function() {
         var mobileMenuTrigger = $('.navigation-trigger a:last-child');
         if(utilities.IsExisty(mobileMenuTrigger)){
             mobileMenuTrigger.click(function(ev) {
@@ -47,8 +46,7 @@
 
     utilities.MoveExistingImagesToContainerBackgroundCover();
 
-
-    $(window).on('load',function(){
+    $(window).on('load', function() {
         // articleContent.each(function(){
         //     var tempHight=  $(this).height();
         //     if(tempHight>highestOne){
@@ -62,11 +60,30 @@
 
     });
 
-    $(window).resize($.throttle(400,function(){
+    $(window).resize($.throttle(400, function() {
         utilities.EqualizeElementsHeightByRow('.article-title');
         utilities.EqualizeElementsHeightByRow(articleContent);
 
     }));
+
+    loadMoreToggleButton.click(function() {
+
+        // if($('.togggle').hasClass('openned')){
+        //     $(this).html('Load More')
+        // }
+        // else{
+        //     $(this).html('Show Less');
+        // }
+
+        $('.togggle').toggleClass('openned');
+        if($(this).html() === 'Load More'){
+            $(this).html('Show Less');
+        }
+        else {
+            $(this).html('Load More')
+        }
+
+    });
 }());
 
 
